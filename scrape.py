7d025260a8c31843904e2d7accee5392983ac64f
LeJon McGowan <lejonmcgowan@gmail.com>
2015-08-24 00:27:53 -0700

    test block of code to analyze for future parsing

--- scrape.py
+++ scrape.py
@@ -1,14 +1,19 @@
 import urllib2
 from bs4 import BeautifulSoup
-webUrl = 'http://catalog.calpoly.edu/coursesaz/'
-testPage = BeautifulSoup(urllib2.urlopen(webUrl).read());
+webUrl = 'http://catalog.calpoly.edu'
+testPage = BeautifulSoup(urllib2.urlopen(webUrl + "/coursesaz").read());
 testPage.prettify();
 
-print 'received webpage info from \"' + testPage.title.string + '\"';
-print 'list of major programs avalilable: '
+#print 'received webpage info from \"' + testPage.title.string + '\"';
+#print 'list of major programs avalilable: '
 
 table = testPage.find_all('a', class_="sitemaplink");
 
-for link in table:
-	print link.string
+#print "going to " + table[0].get("href");
+
+aero = BeautifulSoup(urllib2.urlopen(webUrl + table[0].get("href")).read());
+
+areocourses = aero.find_all(class_="courseblock");
+
+print areocourses[10];
 
