8c34a992f97200b052d0cea1add2e26fe8a5f5c7
LeJon McGowan <lejonmcgowan@gmail.com>
2015-09-01 22:00:30 -0700

    stuff and stuff

--- CourseNode.py
+++ CourseNode.py
@@ -1,43 +1,40 @@
+import string
 from bs4 import BeautifulSoup
 
+
 class CourseNode(object):
-	courseMajor   = "";
-	courseNumber  = -1;
-	courseName    = "";
-	numUnits      = -1;
-	termsOffered  = [];
-	prerequisites = [];
-	reccommended  = [];
-	description   = "";
-	
-	def createNode(self, soupString): #soupNode should be a navigatable string
-		#print soupString;
-		courseblocktitle = soupString.find("div",class_="courseblocktitle")
-		filter(lambda x: x in string.printable, ourseblocktitle)
-		courseblocktitle = courseblocktitle.p.string.encode('utf-8').strip().split();
-		#print courseblocktitle.p.string.encode('utf-8');
-		self.courseMajor = courseblocktitle[0];
-		print courseblocktitle#.rpartition(". ")[1];
-		self.courseNumber = int(courseblocktitle[1].rpartition(". ")[1]);
-		self.courseName = " ".join(courseblocktitle[2:]);
+    courseMajor = "";
+    courseNumber = -1;
+    courseName = "";
+    numUnits = -1;
+    termsOffered = [];
+    prerequisites = [];
+    reccommended = [];
+    description = "";
 
-		self.numUnits =  int(soupString.find('p',class_="courseblockhours").string.split()[0]);
+    def createNode(self, soupString):  # soupNode should be a navigatable string
+        # print soupString;
+        courseblocktitle = soupString.find("div", class_="courseblocktitle")
+        filter(lambda x: x in string.printable, courseblocktitle.p.string)
+        courseblocktitle = courseblocktitle.p.string.encode('utf-8').strip().split();
+        self.courseMajor = courseblocktitle[0];
+        print courseblocktitle  # .rpartition(". ")[1];
+        self.courseNumber = int(courseblocktitle[1].rpartition(". ")[1]);
+        self.courseName = " ".join(courseblocktitle[2:]);
 
-		courseextendedwrap = soupString.find('div',class_="courseextendedwrap");
-		wrapParagraphs = courseextendedwrap.find_all('p');
-		self.termsOffered = wrapParagraphs[0].string.rpartition(": ")[1];
-		for hyperlink in soupString.find_all('a',class_='bubblelink code'):
-			self.prerequisites.append(hyperlink.string);
+        self.numUnits = int(soupString.find('p', class_="courseblockhours").string.split()[0]);
 
+        courseextendedwrap = soupString.find('div', class_="courseextendedwrap");
+        wrapParagraphs = courseextendedwrap.find_all('p');
+        self.termsOffered = wrapParagraphs[0].string.rpartition(": ")[1];
+        for hyperlink in soupString.find_all('a', class_='bubblelink code'):
+            self.prerequisites.append(hyperlink.string);
 
-		self.description = soupString.find('div',class_=courseblockdesc.string);
+        self.description = soupString.find('div', class_="courseblockdesc");
 
-	def __str__(self):
-		printList = [];
-		printList.append(self.courseMajor + " " + str(self.courseNumber) + ": " + self.courseName);
-		printList.apprend("");
-		printList.append(self.description);
-		return "\n".join(printList);
-
-
-		
+    def __str__(self):
+        printList = [];
+        printList.append(self.courseMajor + " " + str(self.courseNumber) + ": " + self.courseName);
+        printList.apprend("");
+        printList.append(self.description);
+        return "\n".join(printList);
--- scrape.py
+++ scrape.py
@@ -6,22 +6,19 @@ webUrl = 'http://catalog.calpoly.edu'
 testPage = BeautifulSoup(urllib2.urlopen(webUrl + "/coursesaz").read(), "html.parser");
 testPage.prettify();
 
-#print 'received webpage info from \"' + testPage.title.string + '\"';
-#print 'list of major programs avalilable: '
+# print 'received webpage info from \"' + testPage.title.string + '\"';
+# print 'list of major programs avalilable: '
 
 table = testPage.find_all('a', class_="sitemaplink");
 
-#print "going to " + table[0].get("href");
+# print "going to " + table[0].get("href");
 
-#aero = BeautifulSoup(urllib2.urlopen(webUrl + table[0].get("href")).read(), "html.parser");
-#areocourses = aero.find_all(class_="courseblock");
+# aero = BeautifulSoup(urllib2.urlopen(webUrl + table[0].get("href")).read(), "html.parser");
+# areocourses = aero.find_all(class_="courseblock");
 
 aeroTest = BeautifulSoup(open("examplecourseblock.html"), "html.parser");
-
-
 
 testNode = CourseNode();
 testNode.createNode(aeroTest);
 
 print testNode;
-
