738a3911e2d750e5db988c9fb5a4d31427e69d86
LeJon McGowan <lejonmcgowan@gmail.com>
2015-09-02 18:25:05 -0700

    finally parse a course from the site's html.

--- CourseNode.py
+++ CourseNode.py
@@ -15,26 +15,29 @@ class CourseNode(object):
     def createNode(self, soupString):  # soupNode should be a navigatable string
         # print soupString;
         courseblocktitle = soupString.find("div", class_="courseblocktitle")
-        filter(lambda x: x in string.printable, courseblocktitle.p.string)
+        # courseblocktitle.p.string = filter(lambda x: x in string.printable, courseblocktitle.p.string)
         courseblocktitle = courseblocktitle.p.string.encode('utf-8').strip().split();
-        self.courseMajor = courseblocktitle[0];
-        print courseblocktitle  # .rpartition(". ")[1];
-        self.courseNumber = int(courseblocktitle[1].rpartition(". ")[1]);
-        self.courseName = " ".join(courseblocktitle[2:]);
+        self.courseMajor = filter(lambda x: x in string.printable and x not in string.digits, courseblocktitle[0])
+        print courseblocktitle
+        courseblocktitle[0] = filter(lambda x: x in string.digits,
+                                     courseblocktitle[0])  # this should get the number out of the string
+        self.courseNumber = int(courseblocktitle[0]);
+        self.courseName = " ".join(courseblocktitle[1:]);
 
-        self.numUnits = int(soupString.find('p', class_="courseblockhours").string.split()[0]);
+        self.numUnits = int(soupString.find('div', class_="courseblockhours").string.split()[0]);
 
         courseextendedwrap = soupString.find('div', class_="courseextendedwrap");
         wrapParagraphs = courseextendedwrap.find_all('p');
-        self.termsOffered = wrapParagraphs[0].string.rpartition(": ")[1];
+        self.termsOffered = wrapParagraphs[0].string.rpartition(": ")[1]
         for hyperlink in soupString.find_all('a', class_='bubblelink code'):
-            self.prerequisites.append(hyperlink.string);
+            self.prerequisites.append(hyperlink.string)
 
-        self.description = soupString.find('div', class_="courseblockdesc");
+        self.description = soupString.find('div', class_="courseblockdesc").p.string
+        self.description = filter(lambda x: x in string.printable, self.description)
 
     def __str__(self):
-        printList = [];
-        printList.append(self.courseMajor + " " + str(self.courseNumber) + ": " + self.courseName);
-        printList.apprend("");
-        printList.append(self.description);
-        return "\n".join(printList);
+        printList = []
+        printList.append(self.courseMajor + " " + str(self.courseNumber) + ": " + self.courseName)
+        printList.append("")
+        printList.append(self.description)
+        return "\n".join(printList)
