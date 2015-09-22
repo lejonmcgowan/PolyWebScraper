eafd1ee4c150776764b2103acade582337793233
LeJon McGowan <lejonmcgowan@gmail.com>
2015-09-21 22:03:07 -0700

    bootstrap in lib. Also, start of a grid

---
+++ grid.js
@@ -0,0 +1,3 @@
+/**
+ * Created by razen_000 on 9/21/2015.
+ */
--- scrape.py
+++ scrape.py
@@ -3,20 +3,20 @@ from bs4 import BeautifulSoup
 from CourseNode import CourseNode
 
 webUrl = 'http://catalog.calpoly.edu'
-testPage = BeautifulSoup(urllib2.urlopen(webUrl + "/coursesaz").read(), "html.parser");
-testPage.prettify();
+testPage = BeautifulSoup(urllib2.urlopen(webUrl + "/coursesaz").read(), "html.parser")
+testPage.prettify()
 
 # print 'received webpage info from \"' + testPage.title.string + '\"';
 # print 'list of major programs avalilable: '
 
-table = testPage.find_all('a', class_="sitemaplink");
+table = testPage.find_all('a', class_="sitemaplink")
 
 # print "going to " + table[0].get("href");
 
-# aero = BeautifulSoup(urllib2.urlopen(webUrl + table[0].get("href")).read(), "html.parser");
-# areocourses = aero.find_all(class_="courseblock");
+aero = BeautifulSoup(urllib2.urlopen(webUrl + table[10].get("href")).read(), "html.parser")
+aerocourses = aero.find_all(class_="courseblock")
 
-aeroTest = BeautifulSoup(open("examplecourseblock.html"), "html.parser");
+aeroTest = aerocourses[7] # BeautifulSoup(aerocourses[0], "html.parser")
 
 testNode = CourseNode();
 testNode.createNode(aeroTest);
